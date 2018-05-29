bd286e7f562d4af3384dff5b2fb1218289728f7e
Mohamed BOUHAMED <med.bouhamed@live.fr>
2018-05-29 10:18:15 +0200

    last version

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const fetch = require('node-fetch');
 
-function fetchAvatarUrl(userId){
-    return fetch('http://localhost:8080/users/${userId}')
-    .then(response => response.json())
-    .then(data => data.imageUrl)
+async function fetchAvatarUrl(userId){
+    // return fetch('http://localhost:8080/users/${userId}')
+    // .then(response => response.json())
+    // .then(data => data.imageUrl)
+    const response = await fetch(`http://localhost:8080/users/${userId}`);
+    console.log('---------------------');
+    console.log(response);
+    const data = await response.json();
+    console.log('---------------------');
+    console.log(data);
+    console.log('---------------------');
+    console.log(data.imageUrl);
+    return data.imageUrl;
+    
 }
 
-const result = fetchAvatarUrl(123);
+var result = fetchAvatarUrl(123);
 console.log(result);
